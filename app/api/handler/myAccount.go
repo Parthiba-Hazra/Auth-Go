8295c0ec3bcba4fa87d05c873b2eb82b73eb9419
Parthiba-Hazra <parthibahazra@gmail.com>
2022-11-04 23:40:57 +0530

    adding test cases

--- app/api/handler/myAccount.go
+++ app/api/handler/myAccount.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/Parthiba-Hazra/auth-go/models"
 	"github.com/Parthiba-Hazra/auth-go/models/error"
@@ -14,7 +15,7 @@ func (h *Handler) MyAccount(c *gin.Context) {
 	user, present := c.Get("user")
 
 	if !present {
-		log.Printf("Unable to get user details :( reason: %v", c)
+		log.Printf("Unable to get user details :( \n reason: %v", c)
 		err := error.NewServerErr()
 		c.JSON(err.ErrorStatus(), gin.H{
 			"error": err,
@@ -24,4 +25,20 @@ func (h *Handler) MyAccount(c *gin.Context) {
 	}
 
 	id := user.(*models.User).UID
+
+	u, err := h.UserService.Get(c, id)
+
+	if err != nil {
+		log.Printf("Unable to find the user: %v\n err: %v", id, err)
+		e := error.NewErrNotFound("user", id.String())
+
+		c.JSON(e.ErrorStatus(), gin.H{
+			"error": e,
+		})
+		return
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"user": u,
+		})
+	}
 }
---
+++ app/api/models/test/user-service.go
@@ -0,0 +1,30 @@
+package test
+
+import (
+	"context"
+
+	"github.com/Parthiba-Hazra/auth-go/models"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/mock"
+)
+
+type TestService struct {
+	mock.Mock
+}
+
+func (m *TestService) Get(c context.Context, id uuid.UUID) (*models.User, error) {
+
+	var rc0 *models.User
+	r_call := m.Called(c, id)
+
+	if r_call.Get(0) != nil {
+		rc0 = r_call.Get(0).(*models.User)
+	}
+
+	var rc1 error
+	if r_call.Get(1) != nil {
+		rc1 = r_call.Get(1).(error)
+	}
+
+	return rc0, rc1
+}
